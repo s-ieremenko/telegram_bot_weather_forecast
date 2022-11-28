69ac48abcbe5c470c0c9c8e9e2243e13ac5d7c14
s-ieremenko <ieremenko.sveta@gmail.com>
2022-11-28 10:36:44 +0100

    test

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,31 +1,26 @@
 import { sendMessage } from "../../sendMessage.js";
 import { options } from '../../options.js';
 
-export const handler = async (event, context, callback) => {
-    // console.log(event.queryStringParameters);
-    // console.log("Received an update from Telegram!!!!!", event.body);
+export const handler = async (event) => {
 
-    // console.log('ev', event, 'ctx', context, 'cll', callback)
-    const { chat: { id }, text, data } = JSON.parse(event.body).message;
+    console.log("Received an update from Telegram!!!!!", event.body);
+
+    const { chat: { id }, text } = JSON.parse(event.body).message;
     const start = async () => {
-        // console.log(JSON.parse(event.body).data)
 
         if (text === '/start') {
             return sendMessage(id, 'Hi! Welcome to the weather forecast. To get the info enter /info')
         }
         if (text === '/info') {
-            // console.log(event.body)
-            // console.log('q', event.queryStringParameters)
-            return sendMessage(id, 'hi', options.reply_markup)
 
-            // return sendMessage(id, 'callback', `data is ${data}`)
+            return sendMessage(id, 'hi', options.reply_markup)
         }
-        if (event.queryStringParameters) {
-            console.log(queryStringParameters)
-        }
+
+
         return sendMessage(id, 'I do not understand that command')
     }
     await start()
 
     return { statusCode: 200 };
-};
+
+}
--- sendMessage.js
+++ sendMessage.js
@@ -3,18 +3,11 @@ import { tlgUrl } from './constants.js'
 
 export const sendMessage = async (chat_id, text, reply_markup) => {
 
-    const res = await axios.post(tlgUrl, {
+    await axios.post(tlgUrl, {
         chat_id,
         text,
         reply_markup
     })
-    // console.log(res)
+
     return true
 }
-// export const sendOptions = async (chat_id, options) => {
-//     await axios.post(tlgUrl, {
-//         chat_id,
-//         options
-//     })
-//     return true
-// }
