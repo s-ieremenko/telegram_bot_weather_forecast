b16bc834f7ed17b128c934681f406ccab4597ba2
s-ieremenko <ieremenko.sveta@gmail.com>
2022-11-28 12:20:24 +0100

    test

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -4,22 +4,28 @@ import { options } from '../../options.js';
 export const handler = async (event) => {
 
     console.log("Received an update from Telegram!!!!!", event.body);
+    const body = JSON.parse(event.body)
+    if (body.callback_query) {
+        console.log(body.callback_query.data)
+    } else {
 
-    const { chat: { id }, text } = JSON.parse(event.body).message;
-    const start = async () => {
+        const { chat: { id }, text } = body.message;
+        const start = async () => {
 
-        if (text === '/start') {
-            return sendMessage(id, 'Hi! Welcome to the weather forecast. To get the info enter /info')
+            if (text === '/start') {
+                return sendMessage(id, 'Hi! Welcome to the weather forecast. To get the info enter /info')
+            }
+            if (text === '/info') {
+
+                return sendMessage(id, 'hi', options.reply_markup)
+            }
+
+
+            return sendMessage(id, 'I do not understand that command')
         }
-        if (text === '/info') {
-
-            return sendMessage(id, 'hi', options.reply_markup)
-        }
-
-
-        return sendMessage(id, 'I do not understand that command')
+        await start()
     }
-    await start()
+
 
     return { statusCode: 200 };
 
